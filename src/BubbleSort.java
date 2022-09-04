0e9c26293bef9c4548c8b3b6a8b26f3b0b5a1a91
kiordev <kiordev@gmail.com>
2022-09-04 16:06:53 +0300

    Add BubbleSort Practice File

---
+++ src/BubbleSort.java
@@ -0,0 +1,64 @@
+//package com.company;
+//import java.util.Scanner;
+//
+//public class BubbleSort {
+//    int array_size;
+//    Scanner num = new Scanner(System.in);
+//        System.out.println("Введите размер массива: ");
+//    array_size = num.nextInt();
+//    int[] my_array = new int[array_size];
+//        System.out.println("Массив создан, размер: "+ array_size);
+//        System.out.println("Ввод элелементов массива: ");
+//    //Ввод массива
+//        for(int i = 0; i < my_array.length; i++){
+//        System.out.println("Введите элемент массива номер - "+(i+1));
+//        my_array[i] = num.nextInt();
+//    }
+//        System.out.println("Массив my_array: ");
+//        for(int i = 0; i < my_array.length; i++){
+//        System.out.print(my_array[i]+" ");
+//    }
+//    //Предложение по сортировке
+//        System.out.println("Отсортировать массив?");
+//        System.out.println("Возрастание + | Убывание -");
+//    char symbol;
+//    Scanner sybmol_enter = new Scanner(System.in);
+//    symbol = sybmol_enter.next().charAt(0);
+//    int temp = 0;
+//    //Сортировка по возростанию
+//        if(symbol == '+'){
+//        boolean isSorted = false;
+//        while(!isSorted){
+//            isSorted=true;
+//            for(int i=1; i< my_array.length; i++){
+//                if(my_array[i]<my_array[i-1]){
+//                    temp=my_array[i];
+//                    my_array[i]=my_array[i-1];
+//                    my_array[i-1]=temp;
+//                    isSorted=false;
+//                }
+//            }
+//
+//        }
+//        //Сортировка по убыванию
+//    }else if(symbol == '-'){
+//        boolean isSorted = false;
+//        while(!isSorted){
+//            isSorted=true;
+//            for(int i=1; i< my_array.length; i++){
+//                if(my_array[i]>my_array[i-1]){
+//                    temp=my_array[i];
+//                    my_array[i]=my_array[i-1];
+//                    my_array[i-1]=temp;
+//                    isSorted=false;
+//                }
+//            }
+//        }
+//
+//    }
+//    //Вывод массива после сортировки
+//        System.out.println("Массив my_array: ");
+//        for(int i = 0; i < my_array.length; i++){
+//        System.out.print(my_array[i]+" ");
+//    }
+//}
--- src/Main.java
+++ src/Main.java
@@ -3,35 +3,64 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-      System.out.println("Calculator");
-      System.out.println("Choose your action: ");
-      System.out.println("+ - * /");
-      char user_action;
-      Scanner user_action_enter = new Scanner(System.in);
-      user_action = user_action_enter.next().charAt(0);
-      System.out.println("Enter your nums");
-      float a, b;
-      float result;
-      Scanner num = new Scanner(System.in);
-      a = num.nextFloat();
-      b = num.nextFloat();
+        int array_size;
+        Scanner num = new Scanner(System.in);
+        System.out.println("Введите размер массива: ");
+        array_size = num.nextInt();
+        int[] my_array = new int[array_size];
+        System.out.println("Массив создан, размер: "+ array_size);
+        System.out.println("Ввод элелементов массива: ");
+        //Ввод массива
+        for(int i = 0; i < my_array.length; i++){
+            System.out.println("Введите элемент массива номер - "+(i+1));
+            my_array[i] = num.nextInt();
+        }
+        System.out.println("Массив my_array: ");
+        for(int i = 0; i < my_array.length; i++){
+            System.out.print(my_array[i]+" ");
+        }
+        //Предложение по сортировке
+        System.out.println("Отсортировать массив?");
+        System.out.println("Возрастание + | Убывание -");
+        char symbol;
+        Scanner sybmol_enter = new Scanner(System.in);
+        symbol = sybmol_enter.next().charAt(0);
+        int temp = 0;
+        //Сортировка по возростанию
+        if(symbol == '+'){
+            boolean isSorted = false;
+            while(!isSorted){
+                isSorted=true;
+                for(int i=1; i< my_array.length; i++){
+                    if(my_array[i]<my_array[i-1]){
+                        temp=my_array[i];
+                        my_array[i]=my_array[i-1];
+                        my_array[i-1]=temp;
+                        isSorted=false;
+                    }
+                }
 
-      if(user_action=='+'){
-          result = a+b;
-          System.out.println("Result is: "+result);
-      }
-      else if(user_action=='-'){
-          result = a-b;
-          System.out.println("Result is: "+result);
-      }
-      else if(user_action=='*'){
-          result = a*b;
-          System.out.println("Result is: "+result);
-      }
-      else if(user_action=='/'){
-          result = a/b;
-          System.out.println("Result is: "+result);
-      }
+            }
+            //Сортировка по убыванию
+        }else if(symbol == '-'){
+            boolean isSorted = false;
+            while(!isSorted){
+                isSorted=true;
+                for(int i=1; i< my_array.length; i++){
+                    if(my_array[i]>my_array[i-1]){
+                        temp=my_array[i];
+                        my_array[i]=my_array[i-1];
+                        my_array[i-1]=temp;
+                        isSorted=false;
+                    }
+                }
+            }
 
+        }
+        //Вывод массива после сортировки
+        System.out.println("Массив my_array: ");
+        for(int i = 0; i < my_array.length; i++){
+            System.out.print(my_array[i]+" ");
+        }
     }
 }
